c310b5e6f75052528de2c097e8cf4936ded4db30
uidumajogu <idumajogu@gmail.com>
2020-01-09 21:10:32 -0500

    fixed security issue

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -1,14 +1,1 @@
-import * as firebase from 'firebase';
 
-
-const firebaseConfig = firebase.initializeApp({
-    apiKey: "AIzaSyCkXSfbk3a6LpdTUwvQrainQQ9PMAeWEO4",
-    authDomain: "digitalvisitorregister.firebaseapp.com",
-    databaseURL: "https://digitalvisitorregister.firebaseio.com",
-    projectId: "digitalvisitorregister",
-    storageBucket: "digitalvisitorregister.appspot.com",
-    messagingSenderId: "955851912111"
-  });
-//   firebase.initializeApp(firebaseConfig);
-
-export default firebaseConfig;
